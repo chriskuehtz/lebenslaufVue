1c1772ad86feeabb26c4fa4cf49ebebbdf2dbbdc
Chris <chriskuehtz@gmail.com>
2021-02-11 21:28:40 +0100

    refactor heading

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -1,44 +1,21 @@
 <template>
-  <div id="heading">
+  <div class="heading">
     <!--Desktop view-->
-    <div v-if="h < w">
-      <div class="container">
-        <div class="itemleft">
-          <h1 style="padding-bottom:0; margin-bottom:0">Christopher Kühtz</h1>
-          <h4 style="padding-top:0; margin-top:0">Frontend Developer</h4>
-          <h3>Nunnenbeckstraße 32</h3>
-          <h3>90489 Nürnberg</h3>
-          <h3>+4915172109146</h3>
-          <h3>chriskuehtz@gmail.com</h3>
-        </div>
-        <div class="itemright">
-          <img
-            src="../assets/profile.png"
-            alt="Thats supposed to be me"
-            id="profile"
-          />
-        </div>
+    <div class="container">
+      <div class="itemleft order-2" order="2">
+        <h1 style="padding-bottom:0; margin-bottom:0">Christopher Kühtz</h1>
+        <h4 style="padding-top:0; margin-top:0">Frontend Developer</h4>
+        <h3>Nunnenbeckstraße 32</h3>
+        <h3>90489 Nürnberg</h3>
+        <h3>+4915172109146</h3>
+        <h3>chriskuehtz@gmail.com</h3>
       </div>
-    </div>
-    <!--Mobile view-->
-    <div v-else>
-      <div class="container">
-        <div class="itemleft">
-          <img
-            src="../assets/profile.png"
-            alt="Thats supposed to be me"
-            id="profileMobile"
-          />
-        </div>
-        <div class="itemleft">
-          
-          <h1 style="padding-bottom:0; margin-bottom:0">Christopher Kühtz</h1>
-          <h4 style="padding-top:0; margin-top:0">Frontend Developer</h4>
-          <h3>Nunnenbeckstraße 32</h3>
-          <h3>90489 Nürnberg</h3>
-          <h3>+4915172109146</h3>
-          <h3>chriskuehtz@gmail.com</h3>
-        </div>
+      <div :class="breakpoints.sm ? 'itemleft order-1' : 'itemright order-3'">
+        <img
+          :class="breakpoints.sm ? 'profileMobile' : 'profile'"
+          src="../assets/profile.png"
+          alt="Thats supposed to be me"
+        />
       </div>
     </div>
   </div>
@@ -48,6 +25,7 @@
 export default {
   name: "heading",
   components: {},
+  props: { breakpoints: Array },
   data() {
     return {
       h: window.innerHeight,
@@ -58,23 +36,21 @@ export default {
 </script>
 
 <style>
-#heading {
+.heading {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  padding-left: 5%;
-  padding-right: 5%;
-  padding-bottom:2%;
+  padding-bottom: 1rem;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 4rem;
 }
-#profile {
+.profile {
   height: 20vw;
   width: 20vw;
 }
-#profileMobile {
-  height: 80vw;
-  width: 80vw;
+.profileMobile {
+  height: 100%;
+  width: 100%;
 }
 .container {
   display: flex; /* or inline-flex */
@@ -86,5 +62,14 @@ export default {
 }
 .itemright {
   text-align: right;
+}
+.order-1 {
+  order: 1;
+}
+.order-2 {
+  order: 2;
+}
+.order-3 {
+  order: 3;
 }
 </style>
